580b93f7b7888480fef11ab1f4092f8eb7d5aa89
Steve Nguyen <stnguyen@redhat.com>
2018-04-04 01:09:14 +0800

    testing jenkins pipeline

--- samplewar/src/main/java/com/sample/HelloServlet.java
+++ samplewar/src/main/java/com/sample/HelloServlet.java
@@ -12,32 +12,25 @@ import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@WebServlet("/put")
-public class AMQPut extends HttpServlet {
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
     /**
 	 * 
 	 */
 	private static final long serialVersionUID = 998638599071628431L;
-	private static final Logger logger = LoggerFactory.getLogger(AMQPut.class);
-	
-	@Inject
-	SampleProducer producer;
+	private static final Logger logger = LoggerFactory.getLogger(HelloServlet.class);
 
     public void init() throws ServletException {
         // Do required initialization
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        logger.info("AMQ put started");
-        try {
-        	String messageTxt = request.getParameter("message");
-        	System.out.println("messageTxt=" + messageTxt);
-			producer.sendMessage(request.getParameter("message"));
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-        
+        logger.info("Received your request at: " + new java.util.Date());   
+        PrintWriter out = response.getWriter();
+        out.write("Received your request at: " + new java.util.Date());
     }
 
     public void destroy() {
